14a72b690b7e6b4fd3bd3620be0f52e69491005d
Kefa698 <kefaombongi07@gmail.com>
2022-05-18 20:23:09 +0300

    service
    Signed-off-by: Kefa698 <kefaombongi07@gmail.com>

--- components/Services.js
+++ components/Services.js
@@ -6,18 +6,20 @@ import Image from 'next/image';
 const Services = ({ services }) => {
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.title}>what we can do </h1>
+			<h1 className={styles.title}>What we can do </h1>
 			<h1 className={styles.subtitle}>Services we can help you with</h1>
 			<div className={styles.services}>
 				{services.map((service) => (
 					<Link passHref key={service.id} href={`/products/${service.name}`}>
 						<div className={styles.service}>
+							<div className={styles.desc}>{service.desc}</div>
 							<span className={styles.cat}>{service.title}</span>
 							<div className={styles.media}>
 								{service.video ? (
 									<video
 										src={`/img/${service.video}`}
 										autoPlay
+										muted
 										loop
 										className={styles.video}
 									/>
